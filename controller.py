ac99a4cf51090111e5483d0570c4b65a0fd75e85
KuiMing <benjamin0901@gmail.com>
2020-07-22 18:50:53 +0800

    set limitation of temperature

--- controller.py
+++ controller.py
@@ -13,33 +13,29 @@ def initial():
     temperature = os.getenv('TEMPERATURE')
     return render_template('controller.html', result=temperature)
 
-@app.route('/power')
-def power():
-    on_off = int(os.getenv('POWER'))
-    if on_off:
-        os.system('say power off')
-        os.environ["POWER"] = "0"
-    else:
-        os.system('say power on')
-        os.environ["POWER"] = "1"
-    return redirect(url_for('initial'))
+def temp_limit(temp):
+    if temp < 18:
+        return 18
+    if temp > 30:
+        return 30
+    return temp
 
 @app.route('/add')
 def add_temperature():
     on_off = int(os.getenv('POWER'))
     if on_off:
-        os.system('say temperature up')
         temperature = int(os.getenv('TEMPERATURE'))
-        os.environ['TEMPERATURE'] = str(temperature + 1)
+        os.environ['TEMPERATURE'] = str(temp_limit(temperature + 1))
+        os.system('say temperature up')
     return redirect(url_for('initial'))
 
 @app.route('/minus')
 def minus_temperature():
     on_off = int(os.getenv('POWER'))
     if on_off:
-        os.system('say temperature down')
         temperature = int(os.getenv('TEMPERATURE'))
-        os.environ['TEMPERATURE'] = str(temperature - 1)
+        os.environ['TEMPERATURE'] = str(temp_limit(temperature - 1))
+        os.system('say temperature down')
     return redirect(url_for('initial'))
 
 if __name__ == '__main__':
