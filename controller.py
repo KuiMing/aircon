11f49db88ff2fd5fa47fb3d5bd98b6c3a235558f
KuiMing <benjamin0901@gmail.com>
2020-07-22 23:27:22 +0800

    send IR signal with irsend

--- controller.py
+++ controller.py
@@ -6,12 +6,26 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 os.environ["POWER"] = "0"
-os.environ['TEMPERATURE'] = "28"
+os.environ['TEMPERATURE'] = "26"
+
 
 @app.route('/')
 def initial():
     temperature = os.getenv('TEMPERATURE')
     return render_template('controller.html', result=temperature)
+
+
+@app.route('/power')
+def power():
+    on_off = int(os.getenv('POWER'))
+    if on_off:
+        os.system('irsend SEND_ONCE aircon power_off')
+        os.environ["POWER"] = "0"
+    else:
+        os.system('irsend SEND_ONCE aircon power_on')
+        os.environ["POWER"] = "1"
+    return redirect(url_for('initial'))
+
 
 def temp_limit(temp):
     if temp < 18:
@@ -20,14 +34,16 @@ def temp_limit(temp):
         return 30
     return temp
 
+
 @app.route('/add')
 def add_temperature():
     on_off = int(os.getenv('POWER'))
     if on_off:
         temperature = int(os.getenv('TEMPERATURE'))
         os.environ['TEMPERATURE'] = str(temp_limit(temperature + 1))
-        os.system('say temperature up')
+        # os.system('say temperature up')
     return redirect(url_for('initial'))
+
 
 @app.route('/minus')
 def minus_temperature():
@@ -35,8 +51,9 @@ def minus_temperature():
     if on_off:
         temperature = int(os.getenv('TEMPERATURE'))
         os.environ['TEMPERATURE'] = str(temp_limit(temperature - 1))
-        os.system('say temperature down')
+        # os.system('say temperature down')
     return redirect(url_for('initial'))
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5566)
