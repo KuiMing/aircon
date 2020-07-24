48dab90b997f79de6cc8daa63a945ac0eb33ce45
KuiMing <benjamin0901@gmail.com>
2020-07-24 17:29:33 +0800

    Automatic turn on the air conditioner

--- controller.py
+++ controller.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 os.environ["POWER"] = "0"
 os.environ['TEMPERATURE'] = "26"
 os.environ['POWERFIGURE'] = "start-button"
+os.environ['AIRCONAUTO'] = "0"
 
 @app.route('/indoor_temperature')
 def temperature_measurement():
@@ -22,7 +23,9 @@ def initial():
     temperature = os.getenv('TEMPERATURE')
     _, indoor = Adafruit_DHT.read_retry(11, 27)
     power_figure = os.getenv('POWERFIGURE')
-    return render_template('controller.html', setting=temperature, indoor=indoor, power=power_figure)
+    auto_mode = int(os.getenv('AIRCONAUTO'))
+    mode = 'AUTO' if auto_mode else ""
+    return render_template('controller.html', setting=temperature, indoor=indoor, power=power_figure, mode=mode)
 
 
 @app.route('/power')
@@ -32,10 +35,31 @@ def power():
         os.system('irsend SEND_ONCE aircon power_off')
         os.environ["POWER"] = "0"
         os.environ['POWERFIGURE'] = "start-button"
+        os.environ['AIRCONAUTO'] = "0"
     else:
         os.system('irsend SEND_ONCE aircon power_on')
         os.environ["POWER"] = "1"
         os.environ['POWERFIGURE'] = "start-button-on"
+    return redirect(url_for('initial'))
+
+def temperature_detector():
+    _, indoor = Adafruit_DHT.read_retry(11, 27)
+    while indoor < 28 and os.getenv('POWER') == '0':
+        _, indoor = Adafruit_DHT.read_retry(11, 27)
+        time.sleep(10)
+    os.system('irsend SEND_ONCE aircon power_on')
+    os.environ["POWER"] = "1"
+    
+@app.route('/auto')
+def auto():
+    on_off = int(os.getenv('AIRCONAUTO'))
+    if on_off:
+        os.environ["AIRCONAUTO"] = "0"
+    else:
+        os.environ["AIRCONAUTO"] = "1"
+        os.environ['POWERFIGURE'] = "start-button-on"
+        thread = Thread(target=temperature_detector)
+        thread.start()
     return redirect(url_for('initial'))
 
 def temp_limit(temp):
@@ -44,7 +68,6 @@ def temp_limit(temp):
     if temp > 30:
         return 30
     return temp
-
 
 @app.route('/add')
 def add_temperature():
